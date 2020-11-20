93492474d3b4dd6cc751f71fc4d18fce19e2d127
Vinicius Petrelli Cicerone <vinicius.petrellicicerone@wmich.edu>
2020-11-19 21:09:25 -0500

    initial commit

---
+++ StoryGame/Views/StoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryGameApp.swift
+//  StoryGame
+//
+//  Created by Vinicius Petrelli Cicerone on 11/10/20.
+//
+
+import SwiftUI
+
+@main
+struct StoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
